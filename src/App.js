685032f7df2d8c00af72328c188e93ab88224edd
shivam gurunani <namisha.gurunani173261@gmail.com>
2024-02-10 04:10:58 +0530

    Myntra Project Completed

--- src/App.js
+++ src/App.js
@@ -29,12 +29,14 @@ function App() {
         </div>
       </>
     ) : (
+     < div className='body'>
           <div className="Loginpage">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Google_Drive_icon_%282020%29.svg/2295px-Google_Drive_icon_%282020%29.svg.png" alt="gdrive" />
             <br/>
             <h1 style={{color:"white", fontSize:"2.6rem", fontWeight:"400"}}>Google Drive</h1>
             <br/>  <br/>
             <button onClick={signIn}>Login to Google Drive</button>
+          </div>
           </div>
     )
     }
